851829e194fff9cfa529e0f243d1098068792b62
Andrey Sergeev <andrey@chyuck.com>
2018-10-07 15:49:46 -0400

    Initial project files

---
+++ Durak/Durak/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Durak
+//
+//  Created by Andrey Sergeev on 10/7/18.
+//  Copyright © 2018 Andrey Sergeev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Durak/Durak/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Durak
+//
+//  Created by Andrey Sergeev on 10/7/18.
+//  Copyright © 2018 Andrey Sergeev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
